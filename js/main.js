c46fcb8ec0ee35ccbd7ffdb1aadae70246bd544d
NehadHema <95351750+NehadHema@users.noreply.github.com>
2022-12-28 14:36:46 +0200

    first commit

---
+++ js/main.js
@@ -0,0 +1,10 @@
+let more = document.getElementById('more');
+let moreBtn = document.getElementById('readmore');
+let pa = document.querySelectorAll('.about p');
+
+
+readmore.addEventListener('click',function(){
+    // more.classList.replace('d-none' , 'd-block');
+    more.style.display= 'block';
+    
+})
